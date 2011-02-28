22512ef24093518fe0a1d30f6cbfe46bd7466064
Andrea <24erre@gmail.com>
2011-02-28 16:06:18 +0100

    Added PathTemplate search

--- tools.py
+++ tools.py
@@ -1,4 +1,8 @@
 import logging
+from django.conf import settings
+import os.path
+import json
+from urlparse import urlsplit
 
 class S2SData(object):
     def __init__(self, data, url, method, content_type):
@@ -21,29 +25,35 @@ class S2SFactory(object):
         return cls.logger
     get_logger = classmethod(get_logger)
 
-    """
-    partners = {
-        partner_id : {
-            data_instance : <S2SData instance>
-            file_hash     : <last_hash>
-        }
+def get_data(partner_id):
+    (country, campain, flight, creativity) = partner_id.split("_")
+    input_data = {
+        'country'    : country,
+        'campain'    : campain,
+        'flight'     : flight,
+        'creativity' : creativity,
     }
-    """
-    partners = {}
-    def get_data(cls, partner_id):
-        """ 
-        Initializes and returns a receiver instance.
-        """
-        try:
-            last_hash = cls.partners[partner_id]['last_hash']
-            #verifica hash
-            #if update data_instance/file_hash
-        except KeyError:
-            #leggi file
-            #update receiver/hash
-            cls.partners[partner_id] = {
-                'data_instance' : S2SData('us=#UC#', 'GET', 'www.partner.net', ''),
-                'last_hash'     : '1234',
-            }
-        return cls.partners[partner_id]['data_instance'] 
-    get_data = classmethod(get_data)
+    for template in ( settings.CAMPAIN_TEMPLATE_CRT,
+                      settings.CAMPAIN_TEMPLATE_FLT,
+                      settings.CAMPAIN_TEMPLATE_CMP, ):
+        file_name = template % input_data
+        if os.path.exists.(file_name) and os.path.isfile(file_name):
+            fh = open(file_name, 'r')
+            content = fh.read()
+            try:
+                raw_data = json.loads(content)
+                url = raw_data['url']
+                tt  = raw_data['track_type']
+            except KeyValue:
+                return None
+    return None
+
+def rd2d(track_type, url):
+    if track_type = 1:
+        o = urlsplit(url)
+        return S2SData(
+            data   = o.query,
+            url    = o.scheme + '://' + o.netloc + o.path,
+            method = 'GET',
+            content_type = '',
+        )
